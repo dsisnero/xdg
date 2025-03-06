c4f17d03dbf5dd35e0154bd5ac49d913e136a4ef
Dominic Sisneros <dsisnero@gmail.com>
2025-03-06 13:17:36 -0700

    refactor: Remove SpecHelpers module and runtime dir creation from XDG spec

--- spec/xdg_spec.cr
+++ spec/xdg_spec.cr
@@ -2,18 +2,7 @@ require "./spec_helper"
 
 describe XDG do
   # Specialized helper for runtime directory creation
-  module SpecHelpers
-    def create_runtime_dir(path : String)
-      parent = File.dirname(path)
-      Dir.mkdir_p(parent) unless Dir.exists?(parent)
-      
-      Dir.mkdir(path)
-      File.chmod(path, 0o700)
-      File.chown(path, uid: Process.uid, gid: Process.gid)
-    end
-  end
 
-  include SpecHelpers
 
   describe "base directory accessors" do
     {% for dir in %w[config data cache state] %}
